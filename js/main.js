fa77714d9f16539f2684b1a6135a0630aebf355d
YefersonAldana <136026404+YefersonAldana@users.noreply.github.com>
2023-06-24 09:35:06 -0700

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,40 @@
+function circulo(){
+    const figura = document.getElementById('figura');
+    figura.classList.toggle('circulo');
+}
+function rectangulo(){
+    const elemento = document.getElementById('figura'); 
+    elemento.classList.toggle('rectangulo');
+}
+function degradado (){
+    const elemento = document.getElementById('figura');
+    elemento.classList.toggle('degradado');
+}
+function ondas (){
+    const elemento = document.getElementById('figura')
+    elemento.classList.toggle('ondas')
+}
+function gif (){
+    const elemento = document.getElementById('figura')
+    elemento.classList.toggle('gif')
+}
+function rombo (){
+    const elemento = document.getElementById('figura')
+    elemento.classList.toggle('rombo')
+}
+function triangulo(){
+    const elemento =document.getElementById('figura')
+    elemento.classList.toggle('triangulo')
+}
+function panel_lateral(){
+    const elemento = document.getElementById('panel-lateral')
+    elemento.classList.toggle('active')
+}
+function cambiar_bg(){
+    const elemento = document.getElementById('body')
+    elemento.classList.toggle('bg2')
+}
+function panel_superior(){
+    const elemento = document.getElementById('panel-superior')
+    elemento.classList.toggle('active2')
+}
